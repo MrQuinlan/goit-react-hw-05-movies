26f22d351c589523e5fbe119bf741c36747cc296
MrQuinlan <ivan.m.kiiashko@gmail.com>
2022-05-30 14:31:28 +0300

    netlify

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import getFilms from '../films-api';
 import s from './Cast.module.css';
+import NoImageFound from '../../img/No-image-found.jpg';
 
 const Cast = () => {
     const [cast, setCast] = useState([]);
@@ -19,17 +20,29 @@ const Cast = () => {
         <ul className={s.list}>
             {cast.map(actor => {
                 const { profile_path, name, character, id } = actor;
-                return (
+                const photo = `https://image.tmdb.org/t/p/original/${profile_path}`;
+
+                return profile_path ? (
                     <li className={s.item} key={id}>
-                        <img
-                            className={s.img}
-                            src={`https://image.tmdb.org/t/p/original/${profile_path}`}
-                            alt={name}
-                        />
+                        <img className={s.img} src={photo} alt={name} />
+                        <p>{name}</p>
+                        <p>Character: {character}</p>
+                    </li>
+                ) : (
+                    <li className={s.item} key={id}>
+                        <img className={s.img} src={NoImageFound} alt={name} />
                         <p>{name}</p>
                         <p>Character: {character}</p>
                     </li>
                 );
+
+                // return (
+                //     <li className={s.item} key={id}>
+                //         <img className={s.img} src={photo} alt={name} />
+                //         <p>{name}</p>
+                //         <p>Character: {character}</p>
+                //     </li>
+                // );
             })}
         </ul>
     );
